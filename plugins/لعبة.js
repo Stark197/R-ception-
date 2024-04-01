e6d74bb6581bca4799918f25c267550bac25d43d
TONY AND SONG <163045349+Stark197@users.noreply.github.com>
2024-04-01 08:20:22 +0000

    Update and rename F.js to لعبة.js

--- plugins/لعبة.js
+++ plugins/لعبة.js
@@ -2,12 +2,10 @@ import fs from 'fs';
 
 let timeout = 60000; // وقت الانتظار بالمللي ثانية (60 ثانية)
 let pointsPerQuestion = 500; // عدد النقاط لكل سؤال
-let correctAnswerAudio = 'shanks.mp3'; // رابط الملف الصوتي للإجابة الصحيحة
 
+// الأمر الرئيسي لبدء اللعبة
 let handler = async (m, { conn }) => {
-    conn.questionsGame = conn.questionsGame || {};
     let id = m.chat;
-    
     if (id in conn.questionsGame) {
         conn.reply(m.chat, '*⌯ ⤹╵ ⌊انتظر حتى ينتهي السؤال الحالي⌉ ┆⎔*', conn.questionsGame[id][0]);
         throw false;
@@ -15,7 +13,6 @@ let handler = async (m, { conn }) => {
 
     let questions = JSON.parse(fs.readFileSync('./src/game/acertijo.json', 'utf8'));
     shuffleArray(questions);
-
     let questionIndex = conn.questionsGame[id] ? conn.questionsGame[id][1] + 1 : 0;
 
     if (questionIndex >= questions.length) {
@@ -26,7 +23,6 @@ let handler = async (m, { conn }) => {
 
     let questionData = questions[questionIndex];
     let question = questionData.question;
-    let answer = questionData.answer;
 
     let caption = `
 ⷮ *السؤال رقم ${questionIndex + 1}:*
@@ -37,27 +33,22 @@ ${question}
 *『🍷┇𝐒𝐔𝐍𝐆 𝐁𝐎𝐓*
 `.trim();
 
-    conn.questionsGame[id] = [
+    conn.questionsGame[m.chat] = [
        await conn.reply(m.chat, caption, m),
        questionIndex,
        setTimeout(async () => {
-           if (conn.questionsGame[id]) await conn.reply(m.chat, `*⌯ ⤹╵ ⌊انتهى الوقت، الإجابة الصحيحة هي: "${answer}"⌉ ┆⎔*`, conn.questionsGame[id][0]);
-           delete conn.questionsGame[id];
+           if (conn.questionsGame[m.chat]) await conn.reply(m.chat, `*⌯ ⤹╵ ⌊انتهى الوقت، الإجابة الصحيحة هي: "${questionData.answer}"⌉ ┆⎔*`, conn.questionsGame[m.chat][0]);
+           delete conn.questionsGame[m.chat];
        }, timeout)
     ];
-    
-    conn.on('message', async (message) => {
-        if (message.text && message.text.toLowerCase() === answer.toLowerCase() && conn.questionsGame[id]) {
-            await conn.sendFile(m.chat, correctAnswerAudio, '', '🎉 مبروك إجابتك الصحيحة! 🎉', m);
-        }
-    });
 };
 
 handler.help = ['لعبة'];
 handler.tags = ['game'];
-handler.command = /^(لعبة)$/i;
+handler.command = /^(كانيكي)$/i;
 export default handler;
 
+// مساعدة: خلط الأسئلة
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
